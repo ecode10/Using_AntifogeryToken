1dc2b3fb5202cadfb415bbab107e1b19626eacf2
Mauricio Junior <mauricio.junior@gmail.com>
2023-02-20 16:16:57 -0600

    add project
    add project

---
+++ TestingUsingAntifogeryToken/Pages/Index.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TestingUsingAntifogeryToken.Pages;
+
+[AutoValidateAntiforgeryToken]
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public void OnGet()
+    {
+
+    }
+}
+
